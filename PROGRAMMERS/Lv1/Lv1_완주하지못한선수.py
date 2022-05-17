0b80116fa72a6117f48bf4f0cb1df42c41e891c0
dbwlgns15 <dbwlgns15@gmail.com>
2022-05-17 11:26:42 +0900

    완주하지못한선수

---
+++ PROGRAMMERS/Lv1/Lv1_완주하지못한선수.py
@@ -0,0 +1,7 @@
+from collections import Counter
+def solution(participant, completion):
+    a = Counter(participant)
+    b = Counter(completion)
+    for i in a:
+        if a[i] != b[i]:
+            return i
